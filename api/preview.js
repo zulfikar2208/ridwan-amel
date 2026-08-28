eedf465ecbf4aac7d9972dfadda47f27814d1d3a
zull <izulfika296@gmail.com>
2026-08-28 20:43:40 +0700

    ga jalan

--- api/preview.js
+++ api/preview.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 function escapeHtml(text) {
-    return text
+    return String(text)
         .replace(/&/g, "&amp;")
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
@@ -12,18 +12,17 @@ function escapeHtml(text) {
 
 module.exports = (req, res) => {
 
-    const nama = req.query.to || "";
-
-    const namaTamu = nama
-        ? decodeURIComponent(nama)
-        : "Bapak/Ibu/Saudara/i";
+    // Ambil nama dari ?to=
+    const namaTamu = req.query.to || "Bapak/Ibu/Saudara/i";
 
     const namaAman = escapeHtml(namaTamu);
 
+    // Ambil index.html
     const filePath = path.join(process.cwd(), "index.html");
 
     let html = fs.readFileSync(filePath, "utf8");
 
+    // Metadata untuk preview WhatsApp
     const metaTags = `
         <meta property="og:type" content="website">
         <meta property="og:title" content="The Wedding of Amel & Ridwan">
@@ -34,10 +33,14 @@ module.exports = (req, res) => {
         <meta name="twitter:card" content="summary_large_image">
         <meta name="twitter:title" content="The Wedding of Amel & Ridwan">
         <meta name="twitter:description" content="Kepada Yth. ${namaAman}">
-        <meta name="twitter:image" content="https://ridwan-amel.vercel.app/img/isi.webp">
+        <meta name="twitter:image" content="https://ridwan-amel.vercel.app/img/isii.webp">
     `;
 
-    html = html.replace("</head>", metaTags + "\n</head>");
+    // Masukkan metadata sebelum </head>
+    html = html.replace(
+        "</head>",
+        metaTags + "\n</head>"
+    );
 
     res.setHeader("Content-Type", "text/html; charset=utf-8");
     res.status(200).send(html);
